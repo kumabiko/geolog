36d90babe39d9bb2b7f5c94f8b24550a00e47ef5
kumabiko <nousbear.dev@gmail.com>
2022-09-23 11:51:36 +0900

    💄 svgアイコンで表示させる

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 import { createStyles, Header, Container, Group, Burger } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import Image from 'next/image';
-import geolog from '@/common/image/geolog.png';
+import GeologIcon from '/public/image/geolog.svg';
 
 const useStyles = createStyles((theme) => ({
   header: {
@@ -73,7 +72,7 @@ export function HeaderSimple({ links }: HeaderSimpleProps) {
   return (
     <Header height={60} mb={120}>
       <Container className={classes.header}>
-        <Image alt="product" src={geolog} width={112} height={32} layout="fixed" />
+        <GeologIcon />
         <Group spacing={5} className={classes.links}>
           {items}
         </Group>
