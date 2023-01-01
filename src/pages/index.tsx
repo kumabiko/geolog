355ffa5a34385b65e2507d00ce1f9a86c4486028
kumabiko <nousbear.dev@gmail.com>
2023-01-01 23:09:48 +0900

    ✨CountryCard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
 import type { NextPage } from 'next';
-import { Layout } from '@/components/Layout';
-import { FirstView } from '@/components/FirstView';
+import { Layout } from '@/components/Layout/Layout';
+import { FirstView } from '@/components/Elements/Hero/FirstView';
+import { Container, SimpleGrid, Space, Title } from '@mantine/core';
+import { CountryCard } from '@/components/Elements';
 
 const Home: NextPage = () => {
   return (
     <Layout title="Home">
       <FirstView />
+      <Container size={1376}>
+        <Space h={40} />
+        <Title order={2}>エリアから探す</Title>
+        <Space h={40} />
+        <SimpleGrid cols={2} spacing="md" breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
+          <CountryCard
+            image="/image/CountryCards/Europe/anthony-delanoix-aDxmYZtYj7g-unsplash.jpg"
+            title="Europe"
+          />
+        </SimpleGrid>
+      </Container>
     </Layout>
   );
 };
