fca057390fff605b7895d51383705866c5a63739
kumabiko <nousbear.dev@gmail.com>
2023-01-02 17:42:13 +0900

    ✨ Footer

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -14,3 +14,71 @@ export const HEADER_LINKS = {
     },
   ],
 };
+
+export const FOOTER_DATA = {
+  data: [
+    {
+      title: 'About',
+      links: [
+        {
+          label: 'Features',
+          link: '#',
+        },
+        {
+          label: 'Pricing',
+          link: '#',
+        },
+        {
+          label: 'Support',
+          link: '#',
+        },
+        {
+          label: 'Forums',
+          link: '#',
+        },
+      ],
+    },
+    {
+      title: 'Project',
+      links: [
+        {
+          label: 'Contribute',
+          link: '#',
+        },
+        {
+          label: 'Media assets',
+          link: '#',
+        },
+        {
+          label: 'Changelog',
+          link: '#',
+        },
+        {
+          label: 'Releases',
+          link: '#',
+        },
+      ],
+    },
+    {
+      title: 'Community',
+      links: [
+        {
+          label: 'Join Discord',
+          link: '#',
+        },
+        {
+          label: 'Follow on Twitter',
+          link: '#',
+        },
+        {
+          label: 'Email newsletter',
+          link: '#',
+        },
+        {
+          label: 'GitHub discussions',
+          link: '#',
+        },
+      ],
+    },
+  ],
+};
